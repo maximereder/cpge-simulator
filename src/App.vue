3f12d2b6bfc591b1326331e1429e6c6e02e36f2a
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-08 19:16:29 +0200

    fix: upgrade transitions for vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div id="app">
-    <div class="app-header">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/whatis">What is ?</router-link>
-      </div>
-    </div>
-    <div class="app-main">
-      <transition name="fade" mode="out-in">
-        <router-view />
-      </transition>
-    </div>
-    <div class="app-footer">
-      Footer
+  <div class="app-header">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/whatis">What is ?</router-link>
     </div>
   </div>
+  <div class="app-main">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+  <div class="app-footer">Footer</div>
 </template>
 
 <style lang="scss">
