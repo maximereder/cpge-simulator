4243c9c06b649313981ec5d9d869e4fea18be6ad
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-17 21:13:11 +0200

    refactor: migrate router to react

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export const App: React.FC = () => {
+  return <Router>
+    <div className={`grid min-h-screen`} style={{ gridTemplateRows: 'minmax(0, auto) minmax(0, 1fr) minmax(0, auto)' }}>
+      <div className="p-4 bg-green-50">
+        <div>
+          <Link to="/">Home</Link> | <Link to="/whatis">What is ?</Link>
+        </div>
+      </div>
+      <div>
+        <Switch>
+          <Route path="/:league/:contest">
+          </Route>
+          <Route path="/whatis">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+      </div>
+      <div className="p-4 bg-green-200">Footer</div>
+    </div>
+  </Router>;
+};
