77a511299f8446b9c7c8784c2971218476d16e8d
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-17 21:11:28 +0200

    chore(app): setup react and remove vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from "path";
-
-const pathSrc = path.resolve(__dirname, "./src");
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: ` @import "${pathSrc}/style/vars.scss"; `
-      }
-    }
-  }
-});
+  plugins: [reactRefresh()]
+})
