cdcd51ccc072af66a7471959f02d07dd0930de97
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-18 02:13:57 +0200

    fix(layout): fix grid layout issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Simulate } from './pages/Simulate';
 
 export const App: React.FC = () => {
   return <Router>
-    <div className={`grid min-h-screen`} style={{ gridTemplateRows: 'minmax(0, auto) minmax(0, 1fr) minmax(0, auto)' }}>
+    <div className={`grid min-h-screen`} style={{ gridTemplate: 'minmax(0, auto) minmax(0, 1fr) minmax(0, auto) / minmax(0, 1fr)' }}>
       <div className="p-4 bg-green-50">
         <div>
           <Link to="/">Home</Link> | <Link to="/whatis">What is ?</Link>
