59eeb7ae5c02af356f94060d3af99e7c69b399dc
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-07-22 14:55:33 +0200

    fix: average function return wrong value

--- src/helpers/average.ts
+++ src/helpers/average.ts
@@ -4,7 +4,7 @@ export const getAverage = (bank: Bank<string>, stats: Record<string, string | un
 	const subjects = Object.keys(bank.factors);
 
 	const statsWithCoefs = subjects.map(subject => {
-		return (Number(stats[subject])) * bank.factors[subject];
+		return (Number(stats[subject]) || 0) * bank.factors[subject];
 	});
 	return statsWithCoefs.reduce((a, b) => (a + b), 0) / subjects.map(s => bank.factors[s]).reduce((a, b) => (a + b), 0);
 }
